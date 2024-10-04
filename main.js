459629c4c2fd765e4aca8fa2b204a89bce0ba23b
tayy0265 <yume_0210@yahoo.com>
2024-10-04 05:58:15 +0000

    10_4 写経

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 function errorInput(){
-      console.log(document.getElementById("month").value)
-      console.log(document.getElementById("day").value)
-
       const month=document.getElementById("month").value;
       const day=document.getElementById("day").value
-
-      console.log("month:",month,"day:"day)
+      if(month<1 || month>12){
+        alert("適切な値ではありません");
+        return
+      }
+      console.log("month:",month,"day:",day);
 }
