44af84c834e4b9ba3624dab41ba2da6bb1cd2075
tayy0265 <yume_0210@yahoo.com>
2025-01-17 06:17:50 +0000

    1_17 数当てゲームの追加

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//function ClickCounterGame(){
+function ClickCounterGame(){
     let count = 0;
 const gameContainer = document.getElementById("game-container");
 
@@ -14,15 +14,19 @@ let counter = document.createElement("p");
 counter.textContent = count;
 
 button1.addEventListener("click",function(){
-        count++;
+    if(count <= 90){
+        count ++;
+         }   
         counter.textContent = count;
     })
 
 button2.addEventListener("click",function(){
-            count=count + 10;
-            counter.textContent = count;
-        })  
-counter.addEventListener("click",function(){
+       if(count <= 90){
+        count = count +10;
+         }      
+  counter.textContent = count;
+    })  
+button3.addEventListener("click",function(){
     count = 0;
     counter.textContent = count;
 })
@@ -32,4 +36,21 @@ gameContainer.appendChild(button1);
 gameContainer.appendChild(button2);
 gameContainer.appendChild(button3);
 gameContainer.appendChild(counter);
-//}
+
+}
+ClickCounterGame();
+
+
+function startNumberGuessGame(){
+    const gameContainer = document.getElementById("game-container");
+    const randomNumber = Math.floor(Math.random() * 100) + 1;
+    let message = document.createElement("p");
+    message.textContent = randomNumber;
+    let input = document.createElement('input');
+    input.type = "number";
+    input.placeholder = "好きな数字を入力してください (1-100)"
+    gameContainer.appendChild(message);
+    gameContainer.appendChild(input);
+}
+
+startNumberGuessGame();
