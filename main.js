c51d2fa2258792cc3edec7d5863e4e514edba78d
tayy0265 <yume_0210@yahoo.com>
2025-01-31 06:18:38 +0000

    1_31 　セレクターの追加

--- main.js
+++ main.js
@@ -1,44 +1,59 @@
-// function ClickCounterGame(){
-//     let count = 0;
-// const gameContainer = document.getElementById("game-container");
+ const gameSelector = document.getElementById("game-select");
+ const gameContainer = document.getElementById("game-container");
+ gameSelector.addEventListener("change",function() {
+    gameContainer.innerHTML = "";
+    switch(gameSelector.value)  {
+        case "number-guess":
+            startNumberGuessGame();
+            break;
+        case "clicker":
+            ClickCounterGame();
+            break;
+    }
+ });
+ 
+ 
+ 
+ function ClickCounterGame(){
+     let count = 0;
+ const gameContainer = document.getElementById("game-container");
 
-// let button1 = document.createElement("button");
-// let button2 = document.createElement("button");
-// let button3 = document.createElement("button");
+let button1 = document.createElement("button");
+ let button2 = document.createElement("button");
+let button3 = document.createElement("button");
 
-// button1.textContent = "+1ボタン";
-// button2.textContent = "+10ボタン";
-// button3.textContent = "リセットボタン"
+ button1.textContent = "+1ボタン";
+ button2.textContent = "+10ボタン";
+ button3.textContent = "リセットボタン"
 
-// let counter = document.createElement("p");
-// counter.textContent = count;
+ let counter = document.createElement("p");
+ counter.textContent = count;
 
-// button1.addEventListener("click",function(){
-//     if(count <= 90){
-//         count ++;
-//          }   
-//         counter.textContent = count;
-//     })
+ button1.addEventListener("click",function(){
+     if(count <= 90){
+        count ++;
+         }   
+        counter.textContent = count;     
+})
 
-// button2.addEventListener("click",function(){
-//        if(count <= 90){
-//         count = count +10;
-//          }      
-//   counter.textContent = count;
-//     })  
-// button3.addEventListener("click",function(){
-//     count = 0;
-//     counter.textContent = count;
-// })
+ button2.addEventListener("click",function(){
+        if(count <= 90){
+        count = count +10;
+          }      
+   counter.textContent = count;
+     })  
+ button3.addEventListener("click",function(){
+    count = 0;
+     counter.textContent = count;
+ })
     
 
-// gameContainer.appendChild(button1);
-// gameContainer.appendChild(button2);
-// gameContainer.appendChild(button3);
-// gameContainer.appendChild(counter);
+ gameContainer.appendChild(button1);
+ gameContainer.appendChild(button2);
+ gameContainer.appendChild(button3);
+ gameContainer.appendChild(counter);
 
-// }
-// ClickCounterGame();
+ }
 
 
 function startNumberGuessGame(){
@@ -68,8 +83,7 @@ function startNumberGuessGame(){
     gameContainer.appendChild(button);
     let count = 0;
     const countDisplay = document.createElement("p");
-    countDisplay,textContent = "試行回数";
+    countDisplay,textContent =  '試行回数 : ${count}';
     gameContainer.appendChild(countDisplay)
 }
 
-startNumberGuessGame();
